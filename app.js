29a15176a776ef74c291d787f9464ef598e76133
nhan nguyen <nhan.nguyen@bliot.io-at-447806605787>
2019-11-12 11:17:09 +0200

    update

--- app.js
+++ app.js
@@ -67,8 +67,7 @@ function callback(error, response, body) {
 var options = {
     url: 'https://api.feedbackly.com/v1/surveys',
     headers: {
-       // "Authorization": process.env.TOKEN
-       "Authorization": "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcGlfa2V5X2lkIjoiNWQ3ZjdhMWM1YWFjYTQ0YzI4ZTMyNDk0Iiwib3JnYW5pemF0aW9uX2lkIjoiNWJkMDZhMDgxYWQ1ODkxMjVmMzMxYmJlIiwidXNlcl9pZCI6IjVkN2Y3YTBlNWFhY2E0NjFiZWUzMjQ5MyIsImV4cGlyYXRpb25fZGF0ZSI6OTk5OTk5OTk5OTk5OX0.0zJYyx5RNW-6AXaM0Bafjr123Q7Lu3XpVPz4RDW0BEE"
+       "Authorization": process.env.TOKEN
 
     }
 };
