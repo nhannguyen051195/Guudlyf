f1916ecb70d743f224ec47e5bae3496078d8115d
nhan nguyen <nhan.nguyen@bliot.io-at-447806605787>
2019-11-13 17:10:15 +0200

    update

---
+++ public/data.js
@@ -0,0 +1,34 @@
+let data = [{
+    questionId: "1",
+    question: "Minkälaiseksi koet opetustilan lämpötilan?",
+    choices: [{ choiceId: -2, choice: "Kylmää", icon: "icons8-happy-100.png" }, { choiceId: -1, choice: "Viileä", icon: "icons8-neutral-100.png" }, { choiceId: 0, choice: "Sopiva", icon: "icons8-lol-100.png" }, { choiceId: 1, choice: "Lämmin", icon: "icons8-neutral-100.png" }, { choiceId: 2, choice: "Kuuma", icon: "icons8-fire-64.png" },]
+}, {
+    questionId: "2",
+    question: "Minkälaiseksi koet sisäilman raikkauden?",
+    choices: [{ choiceId: -2, choice: "Raikas", icon: "icons8-in-love-64.png" }, { choiceId: -1, choice: "Melko raikas", icon: "icons8-happy-100.png" }, { choiceId: 0, choice: "Ei raikas eikä tunkkainen", icon: "icons8-neutral-100.png" }, { choiceId: 1, choice: "Melko tunkkainen", icon: "icons8-sad-100.png" }, { choiceId: 2, choice: "Tunkkainen", icon: "icons8-silent-64.png" },]
+},
+{
+    questionId: "3",
+    question: "Minkälaiseksi koet sisäilman kosteuden?",
+    choices: [{ choiceId: -2, choice: "Kuiva", icon: "icons8-crying-100.png" }, { choiceId: -1, choice: "Melko kuiva", icon: "icons8-sad-100.png" }, { choiceId: 0, choice: "Ei kuiva eikä kostea", icon: "icons8-neutral-100.png" }, { choiceId: 1, choice: "Melko kosteä", icon: "icons8-sad-100.png" }, { choiceId: 2, choice: "Kostea", icon: "icons8-crying-100.png" },]
+}, {
+    questionId: "4",
+    question: "Minkälaiseksi koet sisäilman hajun?",
+    choices: [{ choiceId: -2, choice: "-2", icon: "icons8-crying-100.png" }, { choiceId: -1, choice: "-1", icon: "icons8-sad-100.png" }, { choiceId: 0, choice: "0", icon: "icons8-neutral-100.png" }, { choiceId: 1, choice: "1", icon: "icons8-happy-100.png" }, { choiceId: 2, choice: "2", icon: "icons8-lol-100.png" },]
+}, {
+    questionId: "5",
+    question: "Minkälaiseksi koet sisäilman puhtauden?",
+    choices: [{ choiceId: -2, choice: "-2", icon: "icons8-crying-100.png" }, { choiceId: -1, choice: "-1", icon: "icons8-sad-100.png" }, { choiceId: 0, choice: "0", icon: "icons8-neutral-100.png" }, { choiceId: 1, choice: "1", icon: "icons8-happy-100.png" }, { choiceId: 2, choice: "2", icon: "icons8-lol-100.png" },]
+}, {
+    questionId: "6",
+    question: "Minkälainen on huoneen valaistus?",
+    choices: [{ choiceId: -2, choice: "Huono", icon: "icons8-crying-100.png" }, { choiceId: -1, choice: "Melko huono", icon: "icons8-sad-100.png" }, { choiceId: 0, choice: "Ei hyvä eikä huono", icon: "icons8-neutral-100.png" }, { choiceId: 1, choice: "Melko hyvä", icon: "icons8-happy-100.png" }, { choiceId: 2, choice: "Hyvä", icon: "icons8-lol-100.png" },]
+}, {
+    questionId: "7",
+    question: "Minkälaiseksi koet huoneen äänimaailman eli akustiikan?",
+    choices: [{ choiceId: -2, choice: "Huono", icon: "icons8-crying-100.png" }, { choiceId: -1, choice: "Melko huono", icon: "icons8-sad-100.png" }, { choiceId: 0, choice: "Ei hyvä eikä huono", icon: "icons8-neutral-100.png" }, { choiceId: 1, choice: "Melko hyvä", icon: "icons8-happy-100.png" }, { choiceId: 2, choice: "Hyvä", icon: "icons8-lol-100.png" },]
+}, {
+    questionId: "8",
+    question: "Minkälaiseksi koet työ- tai oppimiskykysi opetustilassa?",
+    choices: [{ choiceId: -2, choice: "Huono", icon: "icons8-crying-100.png" }, { choiceId: -1, choice: "Melko huono", icon: "icons8-sad-100.png" }, { choiceId: 0, choice: "Ei hyvä eikä huono", icon: "icons8-neutral-100.png" }, { choiceId: 1, choice: "Melko hyvä", icon: "icons8-happy-100.png" }, { choiceId: 2, choice: "Hyvä", icon: "icons8-lol-100.png" },]
+},]
